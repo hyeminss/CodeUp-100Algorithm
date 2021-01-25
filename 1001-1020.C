9f95742614cfd878ef2d58ffd3a0511d1660260c
이혜민 <75013068+hyeminss@users.noreply.github.com>
2021-01-25 16:22:17 +0900

    Update 1001-1020.C

--- 1001-1020.C
+++ 1001-1020.C
@@ -38,3 +38,52 @@ int main(){
 int main(){
     printf("\"!@#$%^&*()\"");
 }
+
+@ 1007
+#include<stdio.h>
+int main(){
+    printf("\"C:\\Download\\hello.cpp\"");
+}
+
+@ 1008
+#include<stdio.h>
+int main(){
+    printf("\u250C\u252C\u2510\n");
+    printf("\u251C\u253C\u2524\n");
+    printf("\u2514\u2534\u2518\n");
+}
+
+@ 1009
+#include<stdio.h>
+int main()
+{
+    int n=15;
+    scanf("%d", &n);
+    printf("%d", n);
+
+    return 0;
+}
+
+@ 1010
+#include<stdio.h>
+int main(){
+    char x='p';
+    scanf("%c", &x);
+    printf("%c", x);
+}
+
+@ 1011
+#include<stdio.h>
+int main(){
+    float n=1.414213;
+    scanf("%f",&n);
+    printf("%f",n);
+}
+
+@ 1012
+#include<stdio.h>
+int main(){
+    int a=1,b=2;
+    scanf("%d %d",&a,&b);
+    printf("%d %d",a,b);
+}
