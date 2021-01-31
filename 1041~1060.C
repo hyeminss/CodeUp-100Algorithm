45f828cb9c45bc3122da940e99c4f26f376711de
이혜민 <75013068+hyeminss@users.noreply.github.com>
2021-01-31 23:23:04 +0900

    Create 1041~1060.C

---
+++ 1041~1060.C
@@ -0,0 +1,55 @@
+@ 1041
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%c",&n);
+    printf("%c",n+1);
+    return 0;
+}
+
+@ 1042
+#include<stdio.h>
+int main(){
+    int a,b;
+    scanf("%d %d", &a,&b);
+    printf("%d", a/b);
+}
+
+@ 1043
+#include<stdio.h>
+int main(){
+    int a,b;
+    scanf("%d %d", &a,&b);
+    printf("%d", a%b);
+    return 0;
+}
+
+@ 1044
+#include<stdio.h>
+int main(){
+    long long n;
+    scanf("%lld",&n);
+    printf("%lld",++n);
+    return 0;
+}
+// ++n : 증감연산자
+
+@ 1045
+#include <stdio.h>
+ 
+int main()
+{
+    long long a,b;
+    
+    scanf("%lld %lld", &a, &b);
+    
+    printf("%lld\n", b+a);
+    printf("%lld\n", a-b);
+    printf("%lld\n", a*b);
+    printf("%lld\n", a/b);
+    printf("%lld\n", a%b);
+    printf("%.2f\n", ((float)a/b));
+   
+    
+    return 0;
+}
